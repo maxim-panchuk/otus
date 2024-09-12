947e4b8a8913da0b9bdd5d9195057502b1106dea
mpanchuk <maksim.panchuk29@gmail.com>
2024-09-12 12:43:40 +0300

    hw3

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,43 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(t string) []string {
+	wordList := strings.Fields(t)
+	m := make(map[string]int)
+
+	for _, word := range wordList {
+		m[word] += 1
+	}
+
+	type kv struct {
+		word  string
+		count int
+	}
+
+	wordFreq := make([]kv, 0, len(m))
+
+	for word, count := range m {
+		wordFreq = append(wordFreq, kv{word, count})
+	}
+
+	sort.Slice(wordFreq, func(i, j int) bool {
+		if wordFreq[i].count == wordFreq[j].count {
+			return wordFreq[i].word < wordFreq[j].word
+		}
+		return wordFreq[i].count > wordFreq[j].count
+	})
+
+	topWords := make([]string, 0, 10)
+	for i := 0; i < len(wordFreq) && i < 10; i++ {
+		if i >= len(wordFreq) {
+			break
+		}
+		topWords = append(topWords, wordFreq[i].word)
+	}
+
+	return topWords
 }
